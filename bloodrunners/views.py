091bd2de058881d31d9583ef0f3f8aade04c2042
nipungarg59 <nipungarg59@gmail.com>
2016-09-27 02:52:21 +0530

    Fixed Encode

--- bloodrunners/views.py
+++ bloodrunners/views.py
@@ -7,8 +7,9 @@ def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
+    # print(str(request.GET))
     if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+        message = 'You searched for: %s' % str(request.GET['q'])
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
